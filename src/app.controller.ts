49b53918a916fb65aa929319c477411f53bd3846
SaJid777 <sajidsalim46@gmail.com>
2021-12-09 16:20:40 +0600

    mvc and handlebars view engine

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('getCustom')
+  @Render('index')
+  getCustom() {
+    return { message: 'Custom world!', message1: 'asdasdasd' };
+  }
+
+  @Get('getCustom2')
+  @Render('index')
+  getCustom2() {
+    return { message: 'Custom world!', message1: 'asdasdasd2', nav: [{ url: "foo", test: true, title: "bar" }, { url: "bar" }] };
   }
 }
