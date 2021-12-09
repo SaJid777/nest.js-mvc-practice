ec72429880b980c886342c590272aed5e31c5e60
SaJid777 <sajidsalim46@gmail.com>
2021-12-09 19:01:17 +0600

    Rest API using DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './Test_module/test.module';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [ TestModule ],
+  imports: [ TestModule, CoursesModule ],
   controllers: [AppController],
   providers: [AppService],
 })
